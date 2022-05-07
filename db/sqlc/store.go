728e4a9fb5f9f192765488021ea50df9ee480582
Taha Ahmadi <tahaahmadimail@protonmail.com>
2022-05-07 10:47:19 +0430

    fix deadlock

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -82,19 +82,38 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.FromAccountID,
-			Amount: arg.Amount.Neg().StringFixed(4),
-		})
-		if err != nil {
-			return err
-		}
-		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.ToAccountID,
-			Amount: arg.Amount.StringFixed(4),
-		})
-		if err != nil {
-			return err
+		if arg.FromAccountID < arg.ToAccountID {
+
+			result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+				ID:     arg.FromAccountID,
+				Amount: arg.Amount.Neg().StringFixed(4),
+			})
+			if err != nil {
+				return err
+			}
+			result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+				ID:     arg.ToAccountID,
+				Amount: arg.Amount.StringFixed(4),
+			})
+			if err != nil {
+				return err
+			}
+		} else {
+			result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+				ID:     arg.ToAccountID,
+				Amount: arg.Amount.StringFixed(4),
+			})
+			if err != nil {
+				return err
+			}
+
+			result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+				ID:     arg.FromAccountID,
+				Amount: arg.Amount.Neg().StringFixed(4),
+			})
+			if err != nil {
+				return err
+			}
 		}
 
 		return nil
